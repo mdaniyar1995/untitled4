219bde8b445e6df30fac6fae34aa5851ae3eeef6
Daniyar <mdaniyar1995@gmail.com>
2022-02-14 21:30:44 +0300

    2

--- src/Zadanie.java
+++ src/Zadanie.java
@@ -6,7 +6,6 @@ public class Zadanie {
         File modified = new File("C:\\Users\\Данияр\\Documents");
         SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss dd.MM.yyyy");
         System.out.println("Последнее изменение: " + dateFormat.format(modified.lastModified()));
-
     }
 }
 
